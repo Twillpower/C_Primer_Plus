6f9221e26da05f2f1ed9b8eb76f58b21ecfaabfa
Twillpower <2118147923@qq.com>
2023-02-17 17:14:06 +0800

    *

--- 3_Data_and_C/3.4_C_language_basic_data_types/f_overflow.c
+++ 3_Data_and_C/3.4_C_language_basic_data_types/f_overflow.c
@@ -2,7 +2,7 @@
 
 int main(void)
 {
-    // loat类型必须至少能表示6位有效数字，且取值范围至少是10-37～10+37
+    // float类型必须至少能表示6位有效数字，且取值范围至少是10-37～10+37
     float toobig = 3.4E38 * 100.0f;
     // 浮点数上溢，赋一个表示无穷大的特定值，而且printf()显示该值为inf或infinity
     printf("%e\n", toobig);
